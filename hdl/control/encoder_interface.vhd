d2f2dcfc35cc138c5fa34784438aa54048a54027
Jonathan Piat <piat.jonathan@gmail.com>
2014-06-15 19:33:12 +0200

    stable encoder interface. Period count should be moved to 32bit at some point to avoid saturation and measure even low speeds

--- hdl/control/encoder_interface.vhd
+++ hdl/control/encoder_interface.vhd
@@ -32,7 +32,7 @@ use IEEE.NUMERIC_STD.ALL;
 --use UNISIM.VComponents.all;
 
 entity encoder_interface is
-generic(FREQ_DIV : positive := 100);
+generic(FREQ_DIV : positive := 100; SINGLE_CHANNEL : boolean := true);
 port(
 	clk, reset : in std_logic ;
 	channel_a, channel_b : in std_logic;
@@ -47,6 +47,9 @@ port(
 end encoder_interface;
 
 architecture Behavioral of encoder_interface is
+
+constant DEBOUNCER_DIV : positive := 100 ;
+
 type enc_states is (IDLE, A_H, B_H, A_L, B_L);
 signal cur_state, next_state : enc_states ;
 
@@ -56,7 +59,59 @@ signal divider_counter : std_logic_vector(15 downto 0);
 signal end_div : std_logic ;
 
 signal inc_counter, dec_counter, valid_cw, valid_ccw, latch_period : std_logic ;
+signal channel_a_deb, channel_b_deb : std_logic ;
+signal debouncer_counter : std_logic_vector(15 downto 0);
 begin
+
+
+
+-- simple debouncer
+
+process(clk, reset)
+begin
+	if reset = '1' then
+		debouncer_counter <= std_logic_vector(to_unsigned(DEBOUNCER_DIV, 16)) ;
+	elsif clk'event and clk = '1' then
+		if debouncer_counter = 0 then	
+				debouncer_counter <= std_logic_vector(to_unsigned(DEBOUNCER_DIV, 16)) ;
+		else
+			debouncer_counter <= debouncer_counter - 1 ;
+		end if ;
+	end if ;
+end process;
+	gen_sing : if SINGLE_CHANNEL = true generate
+			process(clk, reset)
+			begin
+				if reset = '1' then
+					channel_b_deb <= '0' ;
+					channel_a_deb <= '0' ;
+				elsif clk'event and clk = '1' then
+					if debouncer_counter = 0 then	
+							channel_a_deb <= channel_a ;
+							channel_b_deb <= channel_a_deb;
+					end if ;
+				end if ;
+			end process ;
+	end generate ;
+	
+		gen_no_sing : if SINGLE_CHANNEL = false generate
+			process(clk, reset)
+			begin
+				if reset = '1' then
+					channel_b_deb <= '0' ;
+					channel_a_deb <= '0' ;
+				elsif clk'event and clk = '1' then
+					if debouncer_counter = 0 then	
+							channel_a_deb <= channel_a ;
+							channel_b_deb <= channel_b;
+					end if ;
+				end if ;
+			end process ;
+		end generate ;
+
+
+-- end of debouncer
+
 
 process(clk, reset)
 begin
@@ -67,36 +122,36 @@ begin
 	end if ;
 end process ;
 
-process(cur_state, channel_a, channel_b)
+process(cur_state, channel_a_deb, channel_b_deb)
 begin
 	next_state <= cur_state ;
 	case cur_state is
 		when IDLE =>
-			if channel_a = '1' and channel_b='0' then
+			if channel_a_deb = '1' and channel_b_deb='0' then
 				next_state <= A_H ;
 			end if ;
-			if channel_b = '1' and channel_a='0' then
+			if channel_b_deb = '1' and channel_a_deb='0' then
 				next_state <= A_L ;
 			end if ;
 		when A_H =>
-			if channel_a = '1' and channel_b = '1' then
+			if channel_a_deb = '1' and channel_b_deb = '1' then
 				next_state <= B_H ;
 			end if ;
-			if channel_a = '0' and channel_b = '0' then
+			if channel_a_deb = '0' and channel_b_deb = '0' then
 				next_state <= IDLE ;
 			end if ;
 		when B_H =>
-			if channel_a = '0' and channel_b = '1' then
+			if channel_a_deb = '0' and channel_b_deb = '1' then
 				next_state <= A_L ;
 			end if ;
-			if channel_b = '0' and channel_a = '1' then
+			if channel_b_deb = '0' and channel_a_deb = '1' then
 				next_state <= A_H ;
 			end if ;
 		when A_L =>
-			if channel_a = '0' and channel_b = '0' then
+			if channel_a_deb = '0' and channel_b_deb = '0' then
 				next_state <= IDLE ;
 			end if ;
-			if channel_a = '1' and channel_b = '1' then
+			if channel_a_deb = '1' and channel_b_deb = '1' then
 				next_state <= B_H ;
 			end if ;
 		when others => next_state <= IDLE ;
@@ -104,15 +159,15 @@ begin
 end process ;
 
 
-inc_counter <= '1' when cur_state = IDLE and channel_a = '1' else
+inc_counter <= '1' when cur_state = IDLE and channel_a_deb = '1' and channel_b_deb = '0' else
 					'0' ;
 					
-dec_counter <= '1' when cur_state = A_H and channel_a = '0' else
+dec_counter <= '1' when cur_state = A_H and channel_a_deb = '0' and channel_b_deb = '0' else
 					'0' ;
 					
 					
-latch_period <= '1' when cur_state = IDLE and channel_a = '1' else
-					  '1' when cur_state = IDLE and channel_b = '1' else
+latch_period <= '1' when cur_state = IDLE and channel_a_deb = '1' else
+					  '1' when cur_state = IDLE and channel_b_deb = '1' else
 					  '0' ;				
 
 
@@ -123,27 +178,27 @@ begin
 		valid_ccw <= '0' ;
 	elsif clk'event and clk='1' then
 		
-		if cur_state = IDLE and channel_a='1' then
+		if cur_state = IDLE and channel_a_deb='1' then
 			valid_cw <= '1' ;
-		elsif cur_state = A_H and channel_a='0' then
+		elsif cur_state = A_H and channel_a_deb='0' then
 			valid_cw <= '0' ;
-		elsif cur_state = B_H and channel_b='0' then
+		elsif cur_state = B_H and channel_b_deb='0' then
 			valid_cw <= '0' ;
-		elsif cur_state = A_L and channel_a='1' then
+		elsif cur_state = A_L and channel_a_deb='1' then
 			valid_cw <= '0' ;
-		elsif cur_state = IDLE and channel_b='1' then
+		elsif cur_state = IDLE and channel_b_deb='1' then
 			valid_cw <= '0' ;
 		end if ;
 		
-		if cur_state = IDLE and channel_b='1' then
+		if cur_state = IDLE and channel_b_deb='1' then
 			valid_ccw <= '1' ;
-		elsif cur_state = A_L and channel_b='0' then
+		elsif cur_state = A_L and channel_b_deb='0' then
 			valid_ccw <= '0' ;
-		elsif cur_state = B_H and channel_a='0' then
+		elsif cur_state = B_H and channel_a_deb='0' then
 			valid_ccw <= '0' ;
-		elsif cur_state = A_H and channel_b='1' then
+		elsif cur_state = A_H and channel_b_deb='1' then
 			valid_ccw <= '0' ;
-		elsif cur_state = IDLE and channel_a='1' then
+		elsif cur_state = IDLE and channel_a_deb='1' then
 			valid_ccw <= '0' ;
 		end if ;
 		
@@ -175,7 +230,7 @@ begin
 	elsif clk'event and clk='1' then
 		if latch_period = '1' then
 			period_counter <= (others => '0') ;
-		elsif end_div = '1' then
+		elsif end_div = '1' and period_counter /= X"7FFF" and period_counter /= X"8000" then
 			period_counter <= period_counter + 1 ;
 		end if ;
 	end if ;
@@ -187,12 +242,15 @@ begin
 		period_latched <= (others => '0') ;
 		pv <= '0' ;
 	elsif clk'event and clk='1' then
-		if latch_period = '1' and ((valid_ccw = '1' and channel_b='1') or (valid_cw = '1' and channel_a='1') ) then
+		if latch_period = '1' and ((valid_ccw = '1' and channel_b_deb='1') or (valid_cw = '1' and channel_a_deb='1') ) then
 			if valid_ccw = '0' and valid_cw = '1' then
 				period_latched <= period_counter ;
 			else
 				period_latched <= (NOT period_counter) + 1 ;
 			end if;
+			pv <= '1' ;
+		elsif period_counter = X"7FFF" or period_counter = X"8000" then
+			period_latched <= X"7FFF" ;
 			pv <= '1' ;
 		else
 			pv <= '0' ;
