37b52111991ecf00489ca3269946bdabacf25c66
Jonathan Piat <piat.jonathan@gmail.com>
2014-09-07 20:47:30 +0200

    adding signals to make interfacing easier

--- hdl/wishbone/peripherals/peripheral_template.vhd
+++ hdl/wishbone/peripherals/peripheral_template.vhd
@@ -1,35 +1,20 @@
--- ----------------------------------------------------------------------
---LOGI-hard
---Copyright (c) 2013, <names> All rights reserved.
---
---This library is free software; you can redistribute it and/or
---modify it under the terms of the GNU Lesser General Public
---License as published by the Free Software Foundation; either
---version 3.0 of the License, or (at your option) any later version.
---
---This library is distributed in the hope that it will be useful,
---but WITHOUT ANY WARRANTY; without even the implied warranty of
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
---Lesser General Public License for more details.
---
---You should have received a copy of the GNU Lesser General Public
---License along with this library.
--- ----------------------------------------------------------------------
 
+-- add you license code here
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-library work ;
-use work.logi_wishbone_peripherals_pack.all ;
+library work;
+use work.logi_utils_pack.all ;
 
-entity <component name> is
-	generic(
-		  wb_size : natural := 16; -- Data port size for wishbone
-	 );
-	 port 
-	 (
+
+entity <component_name> is
+generic(wb_size : positive := 16 ;
+		  memory_size : positive := <your component size in memory>
+		 -- add your component generic parameters starting from here
+		  );
+port(
 		  -- Syscon signals
 		  gls_reset    : in std_logic ;
 		  gls_clk      : in std_logic ;
@@ -41,28 +26,36 @@ entity <component name> is
 		  wbs_cycle      : in std_logic ;
 		  wbs_write     : in std_logic ;
 		  wbs_ack       : out std_logic;
-		 
-		  -- out signals
-		  <your signals goes here>
-	 );
-end <component name>;
+		  
+		  
+		  -- add your component interfaces starting from here
 
-architecture RTL of <component name> is
-	signal read_ack : std_logic ;
-	signal write_ack : std_logic ;
+);
+end <component_name>;
 
-	-- declare your signals here
+architecture RTL of <component_name>
+signal read_ack : std_logic ;
+signal write_ack: std_logic ;
+
+signal component_write, component_read : std_logic ;
+signal component_addr : std_logic_vector(nbit(memory_size)-1 downto 0);
+signal component_write_data,  component_read_data: std_logic_vector(wb_size-1 downto 0);
+
+--declare your component signals here
 
 begin
+
+
 wbs_ack <= read_ack or write_ack;
 
+
+component_write <= wbs_strobe and wbs_write and wbs_cycle ;
 write_bloc : process(gls_clk,gls_reset)
 begin
     if gls_reset = '1' then 
         write_ack <= '0';
     elsif rising_edge(gls_clk) then
-        if ((wbs_strobe and wbs_write and wbs_cycle) = '1' ) then
-            -- complete with what to do on a write
+        if (component_write = '1' ) then
             write_ack <= '1';
         else
             write_ack <= '0';
@@ -70,21 +63,50 @@ begin
     end if;
 end process write_bloc;
 
-
+component_read <= (wbs_strobe and (not wbs_write)  and wbs_cycle) ;
 read_bloc : process(gls_clk, gls_reset)
 begin
     if gls_reset = '1' then
-        
+        read_ack <= '0' ;
+		  wbs_readdata <= (others => '0');
     elsif rising_edge(gls_clk) then
-	 -- complete with what to do on a read
-        if (wbs_strobe = '1' and wbs_write = '0'  and wbs_cycle = '1' ) then
+		  wbs_readdata <= component_read_data ;
+        if component_read = '1' then
             read_ack <= '1';
         else
             read_ack <= '0';
         end if;
-		  
     end if;
 end process read_bloc;
 
-end Behavioral;
+component_addr <= wbs_address(nbit(memory_size)-1 downto 0);
+component_write_data <= wbs_writedata;
 
+-- insert your component code starting from here
+-- use the signal component to control your logic
+
+-- here is an example of a 32bit counter that is controlled by the wishbone bus
+-- writing at address 0 will change the value of the 16 lower bits while writing to address
+-- will change the 16 upper bits
+--counter_bloc : process(gls_clk, gls_reset)
+--begin
+--    if gls_reset = '1' then
+--		  component_count_low <= (others => '0');
+--		  component_count_high <= (others => '0');
+--    elsif rising_edge(gls_clk) then
+--		  if write_component = '1' and component_addr = 0 then
+--			component_count_high <= component_write_data ;
+--		  else
+--			component_count_high <= component_count_high + 1 ;
+--		  end if ;
+--		  if write_component = '1' and component_addr = 1 then
+--			component_count_high <= component_write_data ;
+--		  elsif component_count_high = X"FFFF" then
+--			component_count_high <= component_count_high + 1 ;
+--		  end if ;
+--    end if;
+--end process counter_bloc;
+--component_read_data <= component_count ;
+
+
+end RTL ;
